d23970e448ba0bbc82e16d7389c329b1fb49b303
TheAdnan <adnankicin92@gmail.com>
2018-01-19 10:42:33 +0100

    jquery loaded as content script :tulip:

--- index.js
+++ index.js
@@ -1,24 +1,5 @@
 browser.browserAction.onClicked.addListener(function(tab){
-	var load1 = browser.tabs.executeScript({
-	  file: "bower_components/jquery/dist/jquery.slim.min.js"
-	});
-	load1.then(load_profanity, onError);
-});
-
-
-function load_profanity(result) {
-  	var load2 = browser.tabs.executeScript({
+	browser.tabs.executeScript(null, {
 	  file: "profane.js"
 	});
-
-	load2.then(onSuccess, onError);
-}
-
-function onError(error) {
-  	return;
-}
-
-function onSuccess(result){
-	return;
-}
-
+});
