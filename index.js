6d4884ab9e023f00bf0c799ae622eab3e8109482
TheAdnan <adnankicin92@gmail.com>
2018-01-19 09:59:20 +0100

    remove jQuery.profanity package

--- index.js
+++ index.js
@@ -2,30 +2,23 @@ browser.browserAction.onClicked.addListener(function(tab){
 	var load1 = browser.tabs.executeScript({
 	  file: "bower_components/jquery/dist/jquery.slim.min.js"
 	});
-	load1.then(first_loaded, onError);
+	load1.then(load_profanity, onError);
 });
 
 
-
-function first_loaded(result) {
-  var load2 = browser.tabs.executeScript({
-	  file: "bower_components/jQuery.ProfanityFilter/jquery.profanityfilter.min.js"
-	});
-	load2.then(second_loaded, onError);
-}
-
-function second_loaded(result) {
-  var load3 = browser.tabs.executeScript({
+function load_profanity(result) {
+  	var load2 = browser.tabs.executeScript({
 	  file: "profane.js"
 	});
-	load3.then(success, onError);
+
+	load2.then(onSuccess, onError);
 }
 
 function onError(error) {
   	return;
 }
 
-function success(result){
+function onSuccess(result){
 	return;
 }
 
